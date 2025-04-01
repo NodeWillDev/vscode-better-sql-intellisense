2f3afe63dd33b952c699d86fba7e07ac97073032
NodeWillDev <williamdasilva.dev@gmail.com>
2025-04-01 14:28:52 -0300

    130° commit from 2025

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,10 @@ import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log("Loadded extension");
-  const disposable = vscode.commands.registerCommand("sql-intellisense", () => {
-    vscode.window.showInformationMessage(
-      "Hello i am from VSCODE debug extension"
-    );
-  });
+  const disposable = vscode.commands.registerCommand(
+    "sql-intellisense",
+    () => {}
+  );
   context.subscriptions.push(disposable);
 }
 export function deactivate() {}
