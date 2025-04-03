3e921f5c955266deb3e2a21f1cc19b56d2f7aaba
NodeWillDev <williamdasilva.dev@gmail.com>
2025-04-02 21:12:53 -0300

    137° commit from 2025

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,45 @@ import * as vscode from "vscode";
 import { Connection } from "mysql2/promise";
 
 export function activate(context: vscode.ExtensionContext) {
-  let tables: any = [];
   const disposable = vscode.commands.registerCommand(
     "sql-intellisense",
     async () => {}
   );
+  let decorationType = vscode.window.createTextEditorDecorationType({
+    color: "#569CD6",
+  });
+
+  let updateDecorations = () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
+
+    const text = editor.document.getText();
+    const regex =
+      /(["'`])(?:SELECT|CREATE|INSERT|UPDATE|DELETE|FROM|WHERE|JOIN|ON|LIKE|GROUP BY|ORDER BY|LIMIT)[^"'`]*\1/gi;
+    let ranges: vscode.DecorationOptions[] = [];
+
+    let match;
+    while ((match = regex.exec(text))) {
+      let start = editor.document.positionAt(match.index);
+      let end = editor.document.positionAt(match.index + match[0].length);
+      ranges.push({ range: new vscode.Range(start, end) });
+    }
+
+    editor.setDecorations(decorationType, ranges);
+  };
+
+  let update = vscode.window.onDidChangeActiveTextEditor(updateDecorations);
+  context.subscriptions.push(update);
+
+  vscode.workspace.onDidChangeTextDocument((event) => {
+    if (event.document === vscode.window.activeTextEditor?.document) {
+      updateDecorations();
+    }
+  });
+
+  if (vscode.window.activeTextEditor) {
+    updateDecorations();
+  }
   const intellisense = vscode.languages.registerCompletionItemProvider(
     {
       language: "javascript",
@@ -14,8 +48,11 @@ export function activate(context: vscode.ExtensionContext) {
     },
     {
       async provideCompletionItems(document, position, token, context) {
+        if (/\b(SELECT|FROM)\b/i.test(document.lineAt(position).text))
+          console.log("find");
+        else console.log("NOT FOUND");
         return [
-          new vscode.CompletionItem("log", vscode.CompletionItemKind.File),
+          new vscode.CompletionItem("test", vscode.CompletionItemKind.Color),
         ];
       },
     },
