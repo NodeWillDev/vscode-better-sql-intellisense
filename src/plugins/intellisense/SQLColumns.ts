5d47f8f920cc000c0c7010b7f150f278090ead9f
NodeWillDev <williamdasilva.dev@gmail.com>
2025-04-10 14:40:34 -0300

    144° commit from 2025

--- src/plugins/intellisense/SQLColumns.ts
+++ src/plugins/intellisense/SQLColumns.ts
@@ -1,7 +1,7 @@
 import * as vscode from "vscode";
 
 import { Plugin } from "../Plugin";
-import { COLUMNS, TABLE } from "../../utils/Regex";
+import { TABLE } from "../../utils/Regex";
 import {
   DescribeTablesData,
   LoadRemoteTables,
@@ -47,29 +47,41 @@ export class SQLColumns extends Plugin {
     context: vscode.CompletionContext
   ) {
     const result: vscode.CompletionItem[] = [];
-    let match =
-      /\bSELECT\b\s+(.*?)\s+\bFROM\b\s+[`"'`]?(?<table>\w+)[`"'`]?(?=\s|$)/gim.exec(
-        document.lineAt(position).text
-      );
-    if (match?.groups?.["table"]) this.reference = match?.groups?.["table"];
+
+    let match: RegExpExecArray | null;
     if (
-      this.reference &&
-      /\bSELECT\b(?![\s\S]*\bFROM\b)/i.test(
-        document.lineAt(position).text.substring(0, position.character)
-      )
+      (match =
+        /\b(?:FROM|UPDATE|INTO|DELETE\s+FROM|TRUNCATE|REPLACE|MERGE\s+INTO)\s+[`"'`]?(?<table>\w+)[`"'`]?/.exec(
+          document.lineAt(position).text.substring(0, position.character)
+        )) &&
+      match?.groups?.["table"]
+    )
+      this.reference = match?.groups?.["table"];
+    if (
+      (match =
+        /(?:"'?\s*\bSELECT\b\s*(?!.*\b(FROM|WHERE|VALUES|RETURNING|GROUP|ORDER|HAVING)\b)(?<columns>[\w\s,.*]*)\s*["']?$)|(?:"'?\s*\bUPDATE\b\s+\w+\s+\bSET\b\s+(?!.*\b(WHERE|RETURNING|FROM|GROUP|ORDER|HAVING|JOIN)\b)(?<columns>[^;]+)\s*["']?$)|(?:"'?\s*\bINSERT\s+INTO\b\s+\w+\s*\((?!.*\))(?!.*\b(VALUES|RETURNING)\b)(?<columns>[^)]*)\)?\s*["']?$)/gim.exec(
+          document.lineAt(position).text.substring(0, position.character)
+        )) &&
+      match?.groups?.["columns"]
     ) {
-      this.table
-        ?.find((table) => table.TABLE_NAME === this.reference)
-        ?.data.forEach((data) => {
-          result.push(
-            new vscode.CompletionItem(
-              data["COLUMN_NAME"],
-              vscode.CompletionItemKind.Variable
-            )
-          );
-        });
+      const data = Array.from(
+        (match?.groups?.["columns"] ?? "").matchAll(
+          /(?:^|,\s*)([^=,\s]+)(?=\s*(?:=|,|$))/gim
+        )
+      ).map((colum) => colum[1].trim());
+
+      console.log(data);
     }
 
     return result;
   }
 }
+
+/**
+Eu posso ter a entrada de dois tipos:
+1- 'name, email, password' 
+2- 'name = "...", email = "..."'
+preciso que o split separe esses campos pegando apenas as colunas, por exemplo. 
+se for com update eles são sperados por = eu quero pegar apenas os valores da esquerda, se for um select eles são separados por , 
+entendeu?
+ */
