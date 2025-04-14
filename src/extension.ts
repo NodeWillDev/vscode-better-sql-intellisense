3dbf569865337430f8ae881ce7bbdb576bba8da4
NodeWillDev <williamdasilva.dev@gmail.com>
2025-04-14 11:35:16 -0300

    146° commit from 2025

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,9 @@
 import * as vscode from "vscode";
-import { LoadPlugins } from "./plugins/LoadPlugins";
-import { LoadRemoteTables } from "./load/remote/LoadRemoteTables";
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     "sql-intellisense",
-    async () => {
-      await LoadPlugins.load(context);
-    }
+    async () => {}
   );
   context.subscriptions.push(disposable);
 }
